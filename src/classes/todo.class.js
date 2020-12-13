3f5fc60558630577ec7984adedfc097686d5522b
Victor <vhdiazro@gmail.com>
2020-12-13 01:05:28 -0500

    Update

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -2,11 +2,9 @@ export class Todo {
 
     static fromJson({id, tarea, completado, creado}) {
         const tempTodo = new Todo(tarea);
-
         tempTodo.id = id;
         tempTodo.completado = completado;
         tempTodo.creado = creado;
-
         return tempTodo;
     }
 
